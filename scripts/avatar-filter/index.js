101fc3da89b42be4c7f8a60b4364e06ad1719fae
Ryan Soury <ryan@webdoodle.com.au>
2021-11-04 16:46:09 +1100

    pushed robotjs attempt to its capacity. This library may not work

--- scripts/avatar-filter/index.js
+++ scripts/avatar-filter/index.js
@@ -8,7 +8,7 @@ const path = require("path");
 // const { S3 } = require("aws-sdk");
 const fs = require("fs").promises;
 const chalk = require("chalk");
-// const ono = require("ono");
+const ono = require("ono");
 const mkdirp = require("mkdirp");
 const glob = require("glob-promise");
 const util = require("util");
@@ -17,6 +17,7 @@ const robot = require("robotjs"); // Requires Node 12
 const os = require("os");
 const sharp = require("sharp");
 const del = require("del");
+const debugLogger = require("debug")("avatar-filter");
 
 const options = require("./options");
 
@@ -39,6 +40,8 @@ const outputDir = path.resolve(__dirname, "../../output/step2");
 // const s3Client = new S3({ params: { Bucket: s3BucketName } });
 
 let sourceImages = [];
+
+robot.setKeyboardDelay(100);
 
 if (!s3) {
 	// Create dir
@@ -64,24 +67,26 @@ if (!s3) {
 		// // 3. Remove the green screen background.
 		// 4. Resize the image programmatically to remove the excess background and produce a 720 × 720 -- https://github.com/lovell/sharp
 
-		try {
-			const obs = new OBSWebSocket();
-			await obs.connect({ password: "x5Wh98fTfUauQ9Ms" });
-			console.log(
-				chalk.green(`Success! We're connected & authenticated with OBS.`)
-			);
+		const obs = new OBSWebSocket();
+		await obs.connect({ password: "x5Wh98fTfUauQ9Ms" });
+		console.log(
+			chalk.green(`Success! We're connected & authenticated with OBS.`)
+		);
+		// TODO: Find Snap Camera window coordinates and bring to surface. -- Only needs to be done once.
 
-			// TODO: Find Snap Camera window coordinates and bring to surface. -- Only needs to be done once.
+		const sourceName = `Image`;
+		const snapOutputDir = path.join(os.homedir(), "/Pictures");
+		// Get existing snap output images and mark the start of the next few.
+		const snapImages = await glob(
+			path.join(snapOutputDir, `/Snap Camera Photo*`),
+			{ absolute: true }
+		);
+		debugLogger(`platform: ${process.platform}`);
+		debugLogger(`homedir: ${os.homedir()}`);
+		debugLogger(`Snap output dir: ${snapOutputDir}`);
 
-			const sourceName = `Image`;
-			const snapOutputDir = path.resolve(os.homedir(), "/Pictures");
-			// Get existing snap output images and mark the start of the next few.
-			const snapImages = await glob(
-				path.resolve(snapOutputDir, `/Snap Camera Photo*`),
-				{ absolute: true }
-			);
-
-			for (let i = 0; i < 1; i += 1) {
+		for (let i = 0; i < 1; i += 1) {
+			try {
 				// Set OBS image source file
 				await obs.send("SetSourceSettings", {
 					sourceName,
@@ -91,31 +96,48 @@ if (!s3) {
 				});
 
 				// Use RobotJS to manipulate Snap Camera -- Going to use a Hotkey... so we just need to emulate the keyboard action...
-				robot.keyTap("j", [
-					"shift",
-					process.platform === "darwin" ? "command" : "control" // Command on Mac, Control on Win
-				]);
+				// robot.keyTap("j", [
+				// 	"shift",
+				// 	// process.platform === "darwin" ? "command" : "control" // Command on Mac, Control on Win
+				// 	"control"
+				// ]);
+				// robot.keyToggle("shift", "down");
+				//! Robotjs is not compatible with Snap... or all keypress emulation scripts might not be. Find an alternative... maybe test a golang script.
 
-				await delay(500);
+				await delay(2000);
 
 				const snapIndex = snapImages.length + i;
-				const snapOutputImage = path.resolve(
+				debugLogger(`Snap output image: ${snapIndex}`);
+				const snapOutputImage = path.join(
 					snapOutputDir,
 					`/Snap Camera Photo${snapIndex === 0 ? "" : ` ${snapIndex}`}.jpg`
 				);
+				const outputFile = path.join(outputDir, path.basename(sourceImages[i]));
+
+				debugLogger(`Snap output image: ${snapOutputImage}`);
+
+				try {
+					await fs.access(snapOutputImage, fs.F_OK);
+				} catch (err) {
+					throw ono(err, `Cannot find file ${snapOutputImage}`);
+				}
 
 				// Resize the output file.
-				await sharp(snapOutputImage)
-					.resize(720)
-					.toFile(path.resolve(__dirname, path.basename(sourceImages[i])));
+				await sharp(snapOutputImage).resize(720).toFile(outputFile);
 
 				// Clean the SnapOutput File
 				await del(snapOutputImage);
-			}
 
-			await obs.disconnect();
-		} catch (err) {
-			console.error(chalk.red(util.inspect(err, false, null, true)));
+				console.log(
+					`Successfully filtered image ${path.basename(sourceImages[i])}`
+				);
+			} catch (err) {
+				console.log(chalk.red(`Error! Could not process image`));
+				console.error(util.inspect(err, false, null, true));
+			}
 		}
+
+		await obs.disconnect();
+		console.log(chalk.green(`All done!`));
 	})();
 }
