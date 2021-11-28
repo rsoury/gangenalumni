60bebb6654a075003f80732e4f5b9732fed63d83
Ryan Soury <ryan@webdoodle.com.au>
2021-11-28 15:30:40 +1100

    idnetified the scroll approach

--- pkg/enhance/main.go
+++ pkg/enhance/main.go
@@ -1,11 +1,16 @@
+/*
+	This binary is desiged to accept an image and perform enhancements on said image.
+*/
+
 package main
 
 import (
 	"errors"
 	"fmt"
-	"log"
 	"os"
 	"runtime"
+	"strconv"
+	"time"
 
 	"github.com/go-vgo/robotgo"
 	cli "github.com/spf13/cobra"
@@ -37,40 +42,82 @@ func main() {
 func Enhance(cmd *cli.Command, args []string) {
 	fpid, err := robotgo.FindIds("BlueStacks")
 	if err != nil {
-		fmt.Errorf(err.Error())
+		_ = fmt.Errorf(err.Error())
 	}
 	fmt.Println(fpid)
+	osName := runtime.GOOS
+	currentTs := time.Now().Unix()
+	currentTsStr := strconv.FormatInt(currentTs, 10)
+	err = os.Mkdir("./tmp/"+currentTsStr, 0755) // Create tmp dir for this debug dump
+	if err != nil {
+		_ = fmt.Errorf(err.Error())
+	}
 	err = robotgo.ActivePID(fpid[0])
 	if err != nil {
-		log.Fatal(err)
+		_ = fmt.Errorf(err.Error())
 	}
-	osName := runtime.GOOS
+	// windowX, windowY, windowWidth, windowHeight := robotgo.GetBounds(fpid[0])
+	//* Seems that GetBounds isn't returning the coordinates that are actually correct...
+	// Instead, we're simple going to use the screensize to set the mouse to the center of the screen.
+	screenWidth, screenHeight := robotgo.GetScreenSize()
 
-	log.Println("Start enhancement...")
+	// q.Q(map[string]int{
+	// 	"handle":       robotgo.GetHandle(),
+	// 	"screenWidth":  screenWidth,
+	// 	"screenHeight": screenHeight,
+	// 	"X":            windowX,
+	// 	"Y":            windowY,
+	// 	"width":        windowWidth,
+	// 	"height":       windowHeight,
+	// })
+
+	fmt.Println("Start enhancement...")
 
 	image, _ := cmd.Flags().GetString("image")
 
 	if _, err := os.Stat(image); errors.Is(err, os.ErrNotExist) {
-		log.Fatal("ERROR: Image does not exist")
+		_ = fmt.Errorf("ERROR: Image does not exist")
 	}
+
+	time.Sleep(1 * time.Second) // Just pause to ensure there is a window change.
 
 	screenImg := robotgo.CaptureImg()
 	img, _, err := robotgo.DecodeImg(image)
 	if err != nil {
-		log.Fatal(err)
+		_ = fmt.Errorf(err.Error())
 	}
+	gcv.ImgWrite("./tmp/"+currentTsStr+"/target-image.jpg", img)
+	gcv.ImgWrite("./tmp/"+currentTsStr+"/screen-0.jpg", screenImg)
 
-	log.Println("gcv find image: ")
+	fmt.Println("gcv find image: ")
 	res := gcv.FindAllImg(img, screenImg)
-	yScroll := 250
+	yScroll := 480
 	if osName == "darwin" {
 		yScroll = -1 * yScroll // Scroll up for Mac to go down
 	}
+	count := 1
+	robotgo.MouseSleep = 100
+	centerCoordX := screenWidth / 2
+	centerCoordY := screenHeight / 2
 	for len(res) == 0 {
-		robotgo.Scroll(0, yScroll)
+		countStr := strconv.Itoa(count)
+		// requires that we set the mouse position first.
+		// robotgo.Move(windowX+10, windowY+250)
+		robotgo.Move(centerCoordX, centerCoordY)
+		// robotgo.Scroll(0, yScroll) //* Scrolling does not work on the bluestacks vm
+		// robotgo.ScrollRelative(0, yScroll)
+		robotgo.DragSmooth(centerCoordX, centerCoordY-200)
+		robotgo.Move(centerCoordX, centerCoordY)
+		robotgo.DragSmooth(centerCoordX, centerCoordY-200)
+
+		time.Sleep(1 * time.Second) // Delay each iteration with a buffer
+
 		screenImg = robotgo.CaptureImg()
 		res = gcv.FindAllImg(img, screenImg)
+		gcv.ImgWrite("./tmp/"+currentTsStr+"/screen-"+countStr+".jpg", screenImg)
+		fmt.Println("Scrolling attempt " + countStr)
+		count++
 	}
 	// We now have a result.
-	log.Println(res)
+	fmt.Println(res)
 }
