c5b9884e28991fc686d684a36a74d2f5e4d1d851
Ryan Soury <ryan@webdoodle.com.au>
2021-12-06 03:24:14 +1100

    im about to concede to the idea of using OCR on automatically hypermodified source images... and instead building a library of controls.

--- pkg/enhance/bluestacks_test.go
+++ pkg/enhance/bluestacks_test.go
@@ -36,6 +36,34 @@ func TestGetTextCoordsInImage(t *testing.T) {
 	// Seems greyscaling the image help with OCR.
 	gocv.CvtColor(iMat, &iMat, gocv.ColorBGRToGray)
 
+	iMat.DivideFloat(255)
+	invMat := gocv.NewMat()
+	gocv.Invert(iMat, &invMat, gocv.SolveDecompositionLu)
+	// if gocv.IMWrite(path.Join(cwd, "./tmp/test/TestGetTextCoordsInImage-0.jpg"), iMat) {
+	// 	t.Log("Successfully wrote image to file")
+	// } else {
+	// 	t.Log("Failed to write image to file")
+	// }
+	// kernalMat := gocv.GetStructuringElement(gocv.MorphRect, image.Point{
+	// 	X: 5,
+	// 	Y: 5,
+	// })
+	// dMat := iMat.Clone()
+	// gocv.Dilate(dMat, &dMat, kernalMat)
+	// tMat := dMat.Clone()
+	// gocv.Threshold(dMat, &tMat, 0.15, 0.15, gocv.ThresholdMask)
+	// eMat := gocv.NewMat()
+	// gocv.Divide(iMat, dMat, &eMat)
+	// iMat.CopyToWithMask(&eMat, tMat)
+	// oMat := gocv.NewMat()
+	// gocv.Invert(eMat, &oMat, gocv.SolveDecompositionLu)
+
+	// if gocv.IMWrite(path.Join(cwd, "./tmp/test/TestGetTextCoordsInImage-e.jpg"), oMat) {
+	// 	t.Log("Successfully wrote image to file")
+	// } else {
+	// 	t.Log("Failed to write image to file")
+	// }
+
 	mImg, _ := iMat.ToImage()
 
 	if gocv.IMWrite(path.Join(cwd, "./tmp/test/TestGetTextCoordsInImage.jpg"), iMat) {
