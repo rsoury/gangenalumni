32b14c5a679a6919fade91fa9f87f2ec075b54c4
Ryan Soury <ryan@webdoodle.com.au>
2022-01-17 22:13:44 +1100

    commit the updates to the GetImage Resize Process... but going to revert

--- cmd/bluestacks.go
+++ cmd/bluestacks.go
@@ -6,6 +6,7 @@ import (
 	"image"
 	"log"
 	"math"
+	"sync"
 
 	"github.com/disintegration/imaging"
 	"github.com/go-vgo/robotgo"
@@ -198,47 +199,53 @@ func (b *BlueStacks) GetImageCoordsInImage(searchImg, sourceImg image.Image) (Co
 	// Produce multiple sizes for the search image
 	var res CVResult
 	var sourceImgResizeCache [10]*image.Image
+	var wg sync.WaitGroup
 	for x := 0; x < 4; x++ {
-		resizeSearchWidth := int(math.Round(float64(searchImg.Bounds().Dx()) * (1.0 - float64(x)/10.0)))
-		rSearchImg := imaging.Resize(sourceImg, resizeSearchWidth, 0, imaging.Lanczos)
-		searchMat, _ := gocv.ImageToMatRGB(searchImg)
-		defer searchMat.Close()
+		wg.Add(1)
+		go func(index int) {
+			resizeSearchWidth := int(math.Round(float64(searchImg.Bounds().Dx()) * (1.0 - float64(index)/10.0)))
+			rSearchImg := imaging.Resize(searchImg, resizeSearchWidth, 0, imaging.Lanczos)
+			searchMat, _ := gocv.ImageToMatRGB(searchImg)
+			defer searchMat.Close()
 
-		for i := 0; i < 10; i++ {
-			var rImg image.Image
-			if sourceImgResizeCache[i] == nil {
-				resizeWidth := int(math.Round(float64(sourceImg.Bounds().Dx()) * (1.0 - float64(i)/10.0)))
-				rImg = imaging.Resize(sourceImg, resizeWidth, 0, imaging.Lanczos)
-				sourceImgResizeCache[i] = &rImg
-			} else {
-				rImg = *sourceImgResizeCache[i]
-			}
-			// log.Printf("Resizing image for width %d\n", resizeWidth)
-			// q.Q(i, resizeWidth, rImg.Bounds().Dx())
-			if rImg.Bounds().Dx() <= rSearchImg.Bounds().Dx() || rImg.Bounds().Dy() <= rSearchImg.Bounds().Dy() {
-				// log.Printf("Source image is smaller than search image - Search: %v,%v , Source: %v,%v \n", rImg.Bounds().Dx(), rImg.Bounds().Dy(), searchImg.Bounds().Dx(), searchImg.Bounds().Dy())
-				break // Break the loop if the source image resize becomes smaller than the search image.
-			}
-			// Then process the results to determine the most common coordinate location on the sourceImg
-			srcMat, _ := gocv.ImageToMatRGB(rImg)
-			defer srcMat.Close()
-			if debugMode {
-				gcv.ImgWrite(fmt.Sprintf("./tmp/enhance-debug/%d/screen-resized-%d.jpg", currentTs, i), rImg)
-			}
-			_, confidence, _, topLeftPoint := gcv.FindImgMat(searchMat, srcMat)
+			for i := 0; i < 10; i++ {
+				var rImg image.Image
+				if sourceImgResizeCache[i] == nil {
+					resizeWidth := int(math.Round(float64(sourceImg.Bounds().Dx()) * (1.0 - float64(i)/10.0)))
+					rImg = imaging.Resize(sourceImg, resizeWidth, 0, imaging.Lanczos)
+					sourceImgResizeCache[i] = &rImg
+				} else {
+					rImg = *sourceImgResizeCache[i]
+				}
+				// log.Printf("Resizing image for width %d\n", resizeWidth)
+				if rImg.Bounds().Dx() <= rSearchImg.Bounds().Dx() || rImg.Bounds().Dy() <= rSearchImg.Bounds().Dy() {
+					log.Printf("Source image is smaller than search image - Source: %v,%v , Search: %v,%v \n", rImg.Bounds().Dx(), rImg.Bounds().Dy(), rSearchImg.Bounds().Dx(), rSearchImg.Bounds().Dy())
+					break // Break the loop if the source image resize becomes smaller than the search image.
+				}
+				// Then process the results to determine the most common coordinate location on the sourceImg
+				srcMat, _ := gocv.ImageToMatRGB(rImg)
+				defer srcMat.Close()
+				if debugMode {
+					gcv.ImgWrite(fmt.Sprintf("./tmp/enhance-debug/%d/screen-resized-%d.jpg", currentTs, i), rImg)
+				}
+				_, confidence, _, topLeftPoint := gcv.FindImgMat(searchMat, srcMat)
 
-			r := CVResult{
-				Confidence:  confidence,
-				Point:       topLeftPoint,
-				SearchImage: searchImg,
-				SourceImage: rImg,
-			}
+				r := CVResult{
+					Confidence:  confidence,
+					Point:       topLeftPoint,
+					SearchImage: rSearchImg,
+					SourceImage: rImg,
+				}
 
-			if r.Confidence > res.Confidence && r.Confidence > 0.5 {
-				res = r
+				if r.Confidence > res.Confidence && r.Confidence > 0.5 {
+					res = r
+				}
 			}
-		}
+			wg.Done()
+		}(x)
 	}
+
+	wg.Wait()
 
 	if res.Confidence == 0 {
 		return Coords{}, 0, errors.New("Cannot find image inside of source image")
