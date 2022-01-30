4a72391ad542781f30e32c748ad24ca1a9d9ba9f
Ryan Soury <ryan@webdoodle.com.au>
2022-01-31 00:37:51 +1100

    create a js script to split avatar images by a data point

---
+++ scripts/extras/split-avatars.js
@@ -0,0 +1,91 @@
+/**
+ * Script to split an avatar directory by a specific data point
+ */
+
+require("dotenv").config();
+const path = require("path");
+const chalk = require("chalk");
+const mkdirp = require("mkdirp");
+const debugLog = require("debug")("datasets");
+const jsonfile = require("jsonfile");
+const Queue = require("../utils/queue");
+const options = require("../utils/options")((program) => {
+	program.option(
+		"-o, --output <value>",
+		"An optional path to an output directory. By default the input path is used as the basis for the output path."
+	);
+	program.requiredOption(
+		"-f, --facedata <value>",
+		"Path to the facedata dataset directory."
+	);
+	program.requiredOption(
+		"-b, --by <value>",
+		"Flag to exclude images by. ie. beards"
+	);
+});
+const {
+	inspectObject,
+	stripTrailingSlash,
+	getImages,
+	copy
+} = require("../utils");
+
+const { input, facedata: faceDatasetDir, by } = options;
+const outputDir = stripTrailingSlash(options.output || input);
+
+debugLog(`Output Directory: ${outputDir}`);
+
+// Create dir
+const outputDirBy = path.join(`${outputDir}-split`, by);
+const outputDirBase = `${outputDir}-split/base`;
+mkdirp.sync(outputDirBy);
+mkdirp.sync(outputDirBase);
+
+(async () => {
+	const sourceImages = await getImages(input);
+
+	const q = new Queue(
+		async ({ image }) => {
+			const name = path.basename(image).split(".").slice(0, -1).join(".");
+			const faceFile = path.join(faceDatasetDir, `${name}.json`);
+			const faceData = await jsonfile.readFile(faceFile);
+			const faceDetails = faceData.FaceDetails[0];
+			let outputFile = path.join(outputDirBase, path.basename(image));
+			if (by === "beards") {
+				if (faceDetails.Beard.Value || faceDetails.Mustache.Value) {
+					outputFile = path.join(outputDirBy, path.basename(image));
+				}
+			}
+
+			await copy(image, outputFile);
+
+			return image;
+		},
+		{
+			batchDelay: 200,
+			concurrent: 5
+		}
+	);
+
+	// Queue the images for filtering.
+	sourceImages.forEach((image, i) => {
+		q.push({ image, i });
+	});
+
+	q.on("task_failed", (taskId, err) => {
+		console.log(chalk.red(`[${taskId}] Could not process image`));
+		console.error(inspectObject(err));
+	});
+
+	q.on("task_finish", (taskId, result) => {
+		console.log(`Successfully processed image ${result}`);
+	});
+
+	await new Promise((resolve) => {
+		q.on("drain", () => {
+			resolve();
+		});
+	});
+
+	console.log(chalk.green(`All done!`));
+})();
--- scripts/utils/index.js
+++ scripts/utils/index.js
@@ -1,4 +1,8 @@
 const util = require("util");
+const fs = require("fs").promises;
+const glob = require("glob-promise");
+const path = require("path");
+const { ncp } = require("ncp");
 
 const delay = (timeout) =>
 	new Promise((resolve) => {
@@ -10,6 +14,39 @@ const delay = (timeout) =>
 module.exports.delay = delay;
 
 module.exports.inspectObject = (o) => util.inspect(o, false, null, true);
+
+module.exports.stripTrailingSlash = (str) => {
+	if (str.charAt(str.length - 1) === "/") {
+		return str.substr(0, str.length - 1);
+	}
+	return str;
+};
+
+module.exports.getImages = async (input, extensions = "jpeg,jpg,png") => {
+	let images = [];
+	const stat = await fs.lstat(input);
+	if (stat.isFile()) {
+		// Use file as image
+		images.push(path.resolve(input));
+	} else {
+		// Get images from directory
+		images = await glob(`${input}/*.{${extensions}}`, {
+			absolute: true
+		});
+	}
+
+	return images;
+};
+
+module.exports.copy = async (source, destination, options = {}) =>
+	new Promise((resolve, reject) => {
+		ncp(source, destination, options, (err) => {
+			if (err) {
+				return reject(err);
+			}
+			return resolve();
+		});
+	});
 
 // https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
 function componentToHex(c) {
