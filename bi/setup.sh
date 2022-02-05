1323218a82166f2a60407d3fbe2e526cd480d267
Ryan Soury <ryan@webdoodle.com.au>
2022-02-05 19:47:43 +1100

    remove bi setup chkecout

--- bi/setup.sh
+++ bi/setup.sh
@@ -4,7 +4,6 @@ scriptdir=`dirname "$BASH_SOURCE"`
 
 git clone git@github.com:getredash/redash.git "$scriptdir/redash"
 cd "$scriptdir/redash"
-git checkout v10.1.0
 cp "$scriptdir/.env.example" ./.env
 cat package.json | fx "{ ...this, engines: {} }" > package-new.json && mv package.json package-original.json && mv package-new.json package.json
 yarn
